b0adde441fa53fd1811ea069b1f9803de50ce014
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-03-10 15:38:45 +0100

    Fix a bug in our matching algorithm and implement removing stop words logic

--- rag-search-engine/cli/lib/keyword_search.py
+++ rag-search-engine/cli/lib/keyword_search.py
@@ -2,7 +2,7 @@ import string
 from typing import List
 
 
-from lib.search_utils import tokenize
+from lib.search_utils import tokenize, remove_stop_words
 
 
 # Mapping table from punctuation to None to allow the translate function to remove punctuation
@@ -14,14 +14,20 @@ def keyword_search(
 ) -> List[dict[str, str | int]]:
     result = []
     for el in data["movies"]:
-        # Text Processing
-        search_query_tokens = tokenize(search_query)
-        title_tokens = tokenize(el["title"])
 
+        # Text Processing
+        search_query_tokens = remove_stop_words(tokenize(search_query))
+        title_tokens = remove_stop_words(tokenize(el["title"]))
+
+        found = False
         # If any token from the query matches a token in a title add It to our result
-        for search_token in search_query_tokens:
-            for title_token in title_tokens:
-                if search_token in title_token:
+        for title_token in title_tokens:
+            if found:
+                break
+            for search_query_token in search_query_tokens:
+                if search_query_token in title_token:
+                    # Flag denoting that we have a match
+                    found = True
                     result.append(el)
 
     return result[:limit]
--- rag-search-engine/cli/lib/search_utils.py
+++ rag-search-engine/cli/lib/search_utils.py
@@ -1,12 +1,15 @@
 import sys
 import json
 import string
+from io import TextIOBase
 from pathlib import Path
 from typing import List
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 MOVIES_DB_FILE_PATH = PROJECT_ROOT / "data" / "movies.json"
+
+STOP_WORDS_FILE_PATH = PROJECT_ROOT / "data" / "stopwords.txt"
 
 
 def load_movies_db() -> dict:
@@ -23,6 +26,13 @@ def load_movies_db() -> dict:
     return movies_db
 
 
+def load_stop_words() -> List[str]:
+
+    with open(STOP_WORDS_FILE_PATH, "r") as f:
+        stop_words = f.read().splitlines()
+    return stop_words
+
+
 def tokenize(word: str) -> List[str]:
     # Mapping table from punctuation to None to allow the translate function to remove punctuation
     punc_trans_table = str.maketrans({punc: None for punc in string.punctuation})
@@ -30,3 +40,18 @@ def tokenize(word: str) -> List[str]:
     result = word.lower().translate(punc_trans_table).split(" ")
 
     return result
+
+
+def remove_stop_words(tokens: List[str]) -> List[str]:
+    stop_words = load_stop_words()
+    result = []
+    for token in tokens:
+        if token not in stop_words:
+            result.append(token)
+
+    return result
+
+
+if __name__ == "__main__":
+    print(remove_stop_words(["hello", "the", "out"]))
+    assert set(remove_stop_words(["hello", "the", "out"])) == set(["hello"])
