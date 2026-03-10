b29360d6eaf4dd98b13ab87bbc2c838e7a1d0398
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-03-10 14:29:25 +0100

    implement key word search

--- rag-search-engine/cli/keyword_search_cli.py
+++ rag-search-engine/cli/keyword_search_cli.py
@@ -1,6 +1,37 @@
 #!/usr/bin/env python3
+import json
 
 import argparse
+from pathlib import Path
+from typing import List
+
+# More reliable way for handling paths in python
+parent_directory = Path(__file__).parent.parent
+MOVIES_DB_FILE_PATH =  parent_directory / "data" / "movies.json"
+
+
+def search_in_movies_db(query: str, limit=5) -> List[dict[str, str | int]]:
+    # Initiate the result
+    search_result  = []
+
+    # Load the movies db
+    try:
+        jsonfile = open(MOVIES_DB_FILE_PATH, mode="r", encoding="utf-8")
+        movies_db = json.load(jsonfile)
+        jsonfile.close()
+    except FileNotFoundError:
+        print(f"The file {MOVIES_DB_FILE_PATH} does not exist")
+        return search_result
+    
+    # Itterate over the list of movies
+    for movie in movies_db["movies"]:
+        if query in movie["title"]:
+            search_result.append(movie)
+        
+
+    return search_result[:limit]
+
+
 
 
 def main() -> None:
@@ -14,8 +45,13 @@ def main() -> None:
 
     match args.command:
         case "search":
+            search_query = args.query
             # print the search query here
-            print(f"Searching for: {args.query}")
+            print(f"Searching for: {search_query}")
+
+            search_result = search_in_movies_db(search_query)
+            for i , movie in enumerate(search_result, start=1):
+                print(f"{i}. {movie['title']}")
         case _:
             parser.print_help()
 
