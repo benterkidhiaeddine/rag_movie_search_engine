1ae883dfb8845b5fd23523b0da17a66255e437cf
benterkidhiaeddine <benterki.dhiaeddine@gmail.com>
2026-03-14 16:49:16 +0100

    refactor inverted index logic

--- cli/lib/inverted_index.py
+++ cli/lib/inverted_index.py
@@ -1,7 +1,9 @@
 import os
+import sys
 from typing import List, Set
 from pathlib import Path
-from pickle import dump
+from collections import defaultdict
+from pickle import dump, load
 
 
 from lib.search_utils import tokenize, load_movies_db
@@ -16,28 +18,21 @@ class InvertedIndex:
 
     def __init__(
         self,
-        index: dict[str, Set[int]] = {},
-        docmap: dict[int, str] = {},
     ) -> None:
-        self.index = index
-        self.docmap = docmap
+        self.index: dict[str, Set[int]] = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
+        self.index_path = CACHE_PATH / "index.pkl"
+        self.docmap_path = CACHE_PATH / "docmap.pkl"
 
     def __add_document(self, doc_id: int, text: str) -> None:
         tokens = tokenize(text)
         for token in tokens:
-            if token in self.index:
-                self.index[token].add(doc_id)
-            else:
-                self.index[token] = {doc_id}
+            self.index[token].add(doc_id)
 
     def get_documents(self, term: str):
         term = term.lower()
-        try:
-            result = self.index[term]
-        except KeyError:
-            print("No index was found for the given term")
-            return []
-        result = list(sorted(result))
+
+        result = list(sorted(self.index[term]))
 
         return result
 
@@ -46,20 +41,44 @@ class InvertedIndex:
 
         size = len(movies)
         for i, movie in enumerate(movies, start=1):
-            movie_object = f"{movie["title"]} {movie["description"]}"
+            movie_text = f"{movie["title"]} {movie["description"]}"
             id = movie["id"]
+
             print(f"Adding {id} {movie["title"]} ...")
-            self.__add_document(id, movie_object)
-            self.docmap[id] = movie_object
+            self.__add_document(id, movie_text)
+            self.docmap[id] = movie  # id to document
+
             print(f"{i}/{size} movies added to Index")
 
     def save(self):
         os.makedirs(CACHE_PATH, exist_ok=True)
 
         print("Pickling indexes ...")
-        with open(CACHE_PATH / "index.pkl", "wb") as f:
+        with open(self.index_path, "wb") as f:
             dump(self.index, f)
 
         print("Pickling docmap ...")
-        with open(CACHE_PATH / "docmap.pkl", "wb") as f:
+        with open(self.docmap_path, "wb") as f:
             dump(self.docmap, f)
+
+    def load(self):
+
+        print("Loading indexes ...")
+        try:
+            with open(self.index_path, "rb") as f:
+                self.index = load(f)
+        except FileNotFoundError:
+            print(
+                f"Couldn't find file: {self.index_path} make sure to create it using the build command"
+            )
+            sys.exit(1)
+
+        print("Loading docmap ...")
+        try:
+            with open(self.docmap_path, "rb") as f:
+                self.docmap = load(f)
+        except FileNotFoundError:
+            print(
+                f"Couldn't find file: {self.docmap_path}make sure to create it using the build command"
+            )
+            sys.exit(1)
